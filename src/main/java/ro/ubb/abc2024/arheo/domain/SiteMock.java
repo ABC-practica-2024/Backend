dd5e3d7dcf925f86d682fe4668357735c9d3cbc4
tibumanu <tibu.cristian.sdf@gmail.com>
2024-08-21 16:15:40 +0300

    other: improved Mocks
    added a @ManyToOne to `ArtifactMock` and `SiteMock`; not sure if these work correctly atm;  this is a temporary mock anyhow, we'll see how they'll work with the actual implementations of `Site` and `Artifact`

--- src/main/java/ro/ubb/abc2024/arheo/domain/SiteMock.java
+++ src/main/java/ro/ubb/abc2024/arheo/domain/SiteMock.java
@@ -1,21 +1,22 @@
 package ro.ubb.abc2024.arheo.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
 
 
 // mocking the Site class; won't be used once the actual Site class is implemented
 @Entity
 public class SiteMock {
     @Id
+    @Getter
+    @Setter
+    @Column(name = "id")
     private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
+    @OneToMany(targetEntity = Section.class)
+    @JoinColumn(name = "site_id", referencedColumnName = "id")
+    private List<Section> sections;
 }
