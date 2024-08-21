34cd49f087bb86328c0a9b5dcfe89c5f8677aa79
CalinescuFlorin <florin.calinescu@stud.ubbcluj.ro>
2024-08-21 14:25:57 +0300

    chore: create folder structure

--- src/main/java/ro/ubb/abc2024/arheo/repository/SiteRepository.java
+++ src/main/java/ro/ubb/abc2024/arheo/repository/SiteRepository.java
@@ -1,8 +1,4 @@
 package ro.ubb.abc2024.arheo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import ro.ubb.abc2024.arheo.domain.site.Site;
-
-public interface SiteRepository extends JpaRepository<Site, Long> {
-
+public class SiteRepository {
 }
