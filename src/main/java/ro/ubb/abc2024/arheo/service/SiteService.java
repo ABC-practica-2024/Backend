04a036306fabc0c96600261c7dff4923960cbb51
Mihnea Alexandru Turcu <42068395+BlastOfMihh@users.noreply.github.com>
2024-08-22 16:51:32 +0300

    feat : service + repo
    finished the service and the repo for the Site entity
    
    have a little todo left on the service for the update operation
    also need to add tests

---
+++ src/main/java/ro/ubb/abc2024/arheo/service/SiteService.java
@@ -0,0 +1,15 @@
+package ro.ubb.abc2024.arheo.service;
+
+import ro.ubb.abc2024.arheo.domain.site.Site;
+import ro.ubb.abc2024.arheo.domain.site.SiteStatus;
+
+import java.util.List;
+
+public interface SiteService {
+    public List<Site> getAll();
+    public List<Site> getAllByStatus(SiteStatus status);
+    Site getSiteByTitle(String title);
+    Site addSite(Site site);
+    void deleteSite(Long id);
+    public Site getSite(Long id);
+}
---
+++ src/main/java/ro/ubb/abc2024/arheo/service/SiteServiceImpl.java
@@ -0,0 +1,55 @@
+package ro.ubb.abc2024.arheo.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolationException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ro.ubb.abc2024.arheo.domain.site.Site;
+import ro.ubb.abc2024.arheo.domain.site.SiteStatus;
+import ro.ubb.abc2024.arheo.repository.SiteRepository;
+import ro.ubb.abc2024.utils.exception.UserServiceException;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class SiteServiceImpl implements SiteService{
+    private final SiteRepository siteRepository;
+    public List<Site> getAll(){
+        return siteRepository.findAll();
+    }
+    public List<Site> getAllByStatus(SiteStatus status){
+        return siteRepository.getSitesByStatus(status);
+    }
+    public Site getSiteByTitle(String title){
+        return siteRepository.getSiteByTitle(title);
+    }
+    public Site addSite(Site site){
+        return siteRepository.save(site);
+    }
+    public void deleteSite(Long id){
+        siteRepository.deleteById(id);
+    }
+    public Site getSite(Long id){
+        return siteRepository.getSiteById(id);
+    }
+    // TODO finish this with dtos
+    @Transactional
+    public Site updateSite(long siteId, Site newSite){
+
+        var updateSite = this.siteRepository.findById(siteId).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Site with id %d, does not exist.", siteId)
+                ));
+        updateSite.setTitle(newSite.getTitle());
+        updateSite.setDescription(newSite.getDescription());
+        updateSite.setStatus(newSite.getStatus());
+        updateSite.setMainArchaelogist(newSite.getMainArchaelogist());
+//        try {
+//            validator.validate(user);
+        return this.siteRepository.save(updateSite);
+//        } catch (ConstraintViolationException ex){
+//            throw new UserServiceException(ex.getMessage());
+//        }
+    }
+}
