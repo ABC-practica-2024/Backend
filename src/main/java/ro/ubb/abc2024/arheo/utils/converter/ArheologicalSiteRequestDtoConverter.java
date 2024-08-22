6f845de0becf89b8ff8f854d3ef23d509a443192
NikAlien <115424518+NikAlien@users.noreply.github.com>
2024-08-22 14:21:23 +0300

    fix: create site request, Status variable was changed to automatically get its value

--- src/main/java/ro/ubb/abc2024/arheo/utils/converter/ArheologicalSiteRequestDtoConverter.java
+++ src/main/java/ro/ubb/abc2024/arheo/utils/converter/ArheologicalSiteRequestDtoConverter.java
@@ -3,6 +3,7 @@ package ro.ubb.abc2024.arheo.utils.converter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ro.ubb.abc2024.arheo.domain.CreateArchaeologicalSiteRequest;
+import ro.ubb.abc2024.arheo.domain.Status;
 import ro.ubb.abc2024.arheo.utils.dto.ArheologicalSiteRequestDto;
 import ro.ubb.abc2024.user.UserRepository;
 import ro.ubb.abc2024.utils.converter.Converter;
@@ -17,7 +18,7 @@ public class ArheologicalSiteRequestDtoConverter implements Converter<CreateArch
     public CreateArchaeologicalSiteRequest createFromDto(ArheologicalSiteRequestDto dto) {
         return CreateArchaeologicalSiteRequest.builder()
                 .geograficCoordinates(dto.getGeograficCoordinates())
-                .status(dto.getStatus())
+                .status(Status.PENDING)
                 .createRequestTime(dto.getCreateRequestTime())
                 .archeologist(userRepository.getReferenceById(dto.getArheoId()))
                 .build();
