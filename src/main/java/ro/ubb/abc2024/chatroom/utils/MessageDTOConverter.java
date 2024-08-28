5069f8ccdfb077a696034ffd6364ae0075bac90e
NikAlien <115424518+NikAlien@users.noreply.github.com>
2024-08-28 17:15:31 +0300

    feat: created message dto and converter

--- src/main/java/ro/ubb/abc2024/chatroom/utils/MessageDTOConverter.java
+++ src/main/java/ro/ubb/abc2024/chatroom/utils/MessageDTOConverter.java
@@ -1,4 +1,34 @@
 package ro.ubb.abc2024.chatroom.utils;
 
-public class MessageDTOConverter {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ro.ubb.abc2024.chatroom.domain.Message;
+import ro.ubb.abc2024.chatroom.repository.ChatRoomRepository;
+import ro.ubb.abc2024.chatroom.repository.MessageRepository;
+import ro.ubb.abc2024.user.UserRepository;
+import ro.ubb.abc2024.utils.converter.Converter;
+
+@Component
+@RequiredArgsConstructor
+public class MessageDTOConverter implements Converter<Message, MessageDTO> {
+
+    private final UserRepository userRepository;
+    private final ChatRoomRepository chatRoomRepository;
+
+    @Override
+    public Message createFromDto(MessageDTO dto) {
+        return Message.builder()
+                .content(dto.content())
+                .seenByRecipient(false)
+                .sendTime(dto.sendTime())
+                .chatroom(chatRoomRepository.getReferenceById(dto.chatRoomId()))
+                .sender(userRepository.getReferenceById(dto.senderId()))
+                .recipient(userRepository.getReferenceById(dto.recipientId()))
+                .build();
+    }
+
+    @Override
+    public MessageDTO createFromEntity(Message entity) {
+        return null;
+    }
 }
