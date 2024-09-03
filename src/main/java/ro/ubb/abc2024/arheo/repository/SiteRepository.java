c3a7b74ad692cf9df5ec0f65514e2cd687c0b0b3
Răzvan-Ștefan Florea <razvan.florea@stud.ubbcluj.ro>
2024-09-03 06:46:41 -0700

    feat: refactored get endpoints
    combined the GET endpoints into a single endpoint with optional params
    added the required function inside the service for that endpoint to use the filter params

--- src/main/java/ro/ubb/abc2024/arheo/repository/SiteRepository.java
+++ src/main/java/ro/ubb/abc2024/arheo/repository/SiteRepository.java
@@ -2,6 +2,7 @@ package ro.ubb.abc2024.arheo.repository;
 
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import ro.ubb.abc2024.arheo.domain.artifact.Artifact;
 import ro.ubb.abc2024.arheo.domain.site.Site;
@@ -11,7 +12,7 @@ import ro.ubb.abc2024.user.User;
 import java.beans.Transient;
 import java.util.List;
 
-public interface SiteRepository extends JpaRepository<Site, Long> {
+public interface SiteRepository extends JpaRepository<Site, Long>, JpaSpecificationExecutor<Site> {
     List<Site> getSitesByStatus(SiteStatus status);
     Site getSiteById(Long id);
     Site getSiteByTitle(String title);
