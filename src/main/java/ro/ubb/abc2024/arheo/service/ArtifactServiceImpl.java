42f00cde4b158f16ee6f38316b67d171e8671458
AlisaUrsu <115451781+AlisaUrsu@users.noreply.github.com>
2024-09-04 13:43:40 +0300

    fix: add authorization for artifact endpoints
    Added authorization for the following endpoints:
    for adding an artifacts - before adding it, it checks if the current user that wants to add it is the main archaeologist for that site or is assigned to it
    for editing an artifact - it checks if the archaeologist that wants to edit it is the one that added it before or if he is the main archaeologist
    for deleting an artifact - the logic is the same for this: an artifact can be deleted only by the archaeologist that added it or the main one

--- src/main/java/ro/ubb/abc2024/arheo/service/ArtifactServiceImpl.java
+++ src/main/java/ro/ubb/abc2024/arheo/service/ArtifactServiceImpl.java
@@ -1,17 +1,26 @@
 package ro.ubb.abc2024.arheo.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.transaction.Transactional;
 import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ro.ubb.abc2024.arheo.domain.artifact.Artifact;
+import ro.ubb.abc2024.arheo.domain.section.Section;
+import ro.ubb.abc2024.arheo.domain.site.Site;
 import ro.ubb.abc2024.arheo.exception.ArtifactServiceException;
 import ro.ubb.abc2024.arheo.repository.ArtifactRepository;
 import ro.ubb.abc2024.user.User;
 import ro.ubb.abc2024.utils.validation.GenericValidator;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RequiredArgsConstructor
 @Service
@@ -98,5 +107,52 @@ public class ArtifactServiceImpl implements ArtifactService {
     @Override
     public List<Artifact> getArtifactsByCategory(String category) {
         return artifactRepository.findArtifactsByCategory(category);
+    }
+
+    @Override
+    public Page<Artifact> getAllPaginatedByCriteria(Long siteId, Long sectionId, Long archaeologistId, String label, String category, Boolean analysisCompleted, Pageable pageable) {
+        return artifactRepository.findAll((root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (siteId != null) {
+                Join<Artifact, Section> artifactSectionJoin = root.join("section");
+                Join<Section, Site> sectionSiteJoin = artifactSectionJoin.join("site");
+                predicates.add(criteriaBuilder.equal(sectionSiteJoin.get("id"), siteId));
+            }
+            if (sectionId != null) {
+                Join<Artifact, Section> artifactSectionJoin = root.join("section");
+                predicates.add(criteriaBuilder.equal(artifactSectionJoin.get("id"), sectionId));
+            }
+            if (archaeologistId != null) {
+                Join<Artifact, User> artifactUserJoin = root.join("archeologist");
+                predicates.add(criteriaBuilder.equal(artifactUserJoin.get("id"), archaeologistId));
+            }
+            if (label != null) {
+                predicates.add(criteriaBuilder.like(root.get("label"),  "%" + label + "%"));
+            }
+            if (category != null) {
+                predicates.add(criteriaBuilder.equal(root.get("category"), category));
+            }
+            if (analysisCompleted != null) {
+                predicates.add(criteriaBuilder.equal(root.get("analysisCompleted"), analysisCompleted));
+            }
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        }, pageable);
+    }
+
+    @Override
+    public Long getMainArchaeologistIdFromArtifactId(Long id) {
+        return artifactRepository.findMainArcheologistIdByArtifactId(id);
+    }
+
+    @Override
+    public List<Long> getAssignedArchaeologistIdsBySectionId(Long id) {
+        List<User> archaeologists = artifactRepository.findAssignedArchaeologistIdsBySectionId(id);
+        return archaeologists.stream().map(User::getId).toList();
+    }
+
+    @Override
+    public Long getMainArchaeologistIdFromSectionId(Long id) {
+        return artifactRepository.findMainArcheologistIdBySectionId(id);
     }
 }
