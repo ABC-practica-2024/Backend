018157737b9b0b51674f0bc4ce3f6362869d9947
AlisaUrsu <115451781+AlisaUrsu@users.noreply.github.com>
2024-09-04 13:43:40 +0300

    fix: add authorization for artifact endpoints
    Added authorization for the following endpoints:
    for adding an artifacts - before adding it, it checks if the current user that wants to add it is the main archaeologist for that site or is assigned to it
    for editing an artifact - it checks if the archaeologist that wants to edit it is the one that added it before or if he is the main archaeologist
    for deleting an artifact - the logic is the same for this: an artifact can be deleted only by the archaeologist that added it or the main one

--- src/main/java/ro/ubb/abc2024/arheo/repository/ArtifactRepository.java
+++ src/main/java/ro/ubb/abc2024/arheo/repository/ArtifactRepository.java
@@ -1,6 +1,7 @@
 package ro.ubb.abc2024.arheo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import ro.ubb.abc2024.user.User;
 import java.util.List;
 
 @Repository
-public interface ArtifactRepository extends JpaRepository<Artifact, Long> {
+public interface ArtifactRepository extends JpaRepository<Artifact, Long>, JpaSpecificationExecutor<Artifact> {
     List<Artifact> findArtifactsByLabScanIsNull();
     //List<Artifact> findArtifactsByArcheologist(User archeologist);
     List<Artifact> findArtifactsByArcheologist_Id(Long id);
@@ -18,4 +19,10 @@ public interface ArtifactRepository extends JpaRepository<Artifact, Long> {
     List<Artifact> findArtifactsBySection_Site_Id(Long id);
     List<Artifact> findArtifactsByCategory(String category);
     List<Artifact> findArtifactsBySection_Site_IdAndArcheologist_Id(Long siteId, Long archeologistId);
+    @Query("select a.section.site.mainArchaeologist.id from Artifact a where a.id = :id")
+    Long findMainArcheologistIdByArtifactId(@Param("id") Long id);
+    @Query("select a.section.site.mainArchaeologist.id from Artifact a where a.section.id = :id")
+    Long findMainArcheologistIdBySectionId(@Param("id") Long id);
+    @Query("SELECT a.section.site.archaeologists from Artifact a WHERE a.section.id = :id")
+    List<User> findAssignedArchaeologistIdsBySectionId(@Param("id") Long id);
 }
