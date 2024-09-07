0b2232929f1861d5fe24d947a49cd8e9b5d67487
AlisaUrsu <115451781+AlisaUrsu@users.noreply.github.com>
2024-09-07 19:37:13 +0300

    feat: issue #47 done
    Details in the pull request for this branch

--- src/main/java/ro/ubb/abc2024/arheo/controller/utils/HelperMethods.java
+++ src/main/java/ro/ubb/abc2024/arheo/controller/utils/HelperMethods.java
@@ -1,6 +1,9 @@
 package ro.ubb.abc2024.arheo.controller.utils;
 
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ro.ubb.abc2024.arheo.service.ArtifactService;
+import ro.ubb.abc2024.user.UserService;
 import ro.ubb.abc2024.utils.converter.Converter;
 
 import java.util.HashMap;
@@ -20,6 +23,29 @@ public class HelperMethods {
         response.put("totalPages", page.getTotalPages());
 
         return response;
-
     }
+
+    public static Long getCurrentUserId(UserService userService) {
+        var currentUser = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userService.getUser(currentUser).getId();
+    }
+
+    public static Long getMainArchaeologistIdByArtifactId(ArtifactService artifactService, Long artifactId) {
+        return artifactService.getMainArchaeologistIdFromArtifactId(artifactId);
+    }
+
+    public static Long getMainArchaeologistIdBySectionId(ArtifactService artifactService, Long sectionId) {
+        return artifactService.getMainArchaeologistIdFromSectionId(sectionId);
+    }
+
+    public static List<Long> getAssignedArchaeologists(ArtifactService artifactService, Long sectionId) {
+        return artifactService.getAssignedArchaeologistIdsBySectionId(sectionId);
+    }
+
+    public static boolean isAdmin() {
+        return SecurityContextHolder.getContext().getAuthentication()
+                .getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("SCOPE_ADMIN"));
+    }
+
 }
